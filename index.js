fe131f34f25b1a12c2b5bc5165f5ac620c48d416
Ojoachele Onuh <101713829+Achele@users.noreply.github.com>
2022-07-15 05:13:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const myEmojis = ["👩‍💻", "💃", "👩‍👧‍👦" ]
+const emojiContainer = document.getElementById("emoji-container")
+const emojiInput = document.getElementById("emoji-input")
+const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
+const delBtn = document.getElementById("del-btn")
+
+function renderEmojis() {
+emojiContainer.innerHTML = ""
+for (let i = 0; i < myEmojis.length; i++) {
+    const emoji = document.createElement('span')
+    emoji.textContent = myEmojis[i]
+    emojiContainer.append(emoji)
+}
+}
+
+renderEmojis()
+
+pushBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+unshiftBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+popBtn.addEventListener("click", function() {
+    myEmojis.pop()
+    renderEmojis()
+})
+
+shiftBtn.addEventListener("click", function() {
+    myEmojis.shift()
+    renderEmojis()
+})
+
+delBtn.addEventListener("click", function() {
+    emojiContainer.innerHTML = ""
+    // console.log("delete btn")
+})
